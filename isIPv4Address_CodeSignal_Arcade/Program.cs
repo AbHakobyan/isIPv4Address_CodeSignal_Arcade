f35e92048b494eb10971f8a011f2941acd11f1dc
Albert Hakobyan <abhakobyan24@gmail.com>
2021-09-25 00:20:42 +0400

    Добавьте файлы проекта.

---
+++ isIPv4Address_CodeSignal_Arcade/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isIPv4Address_CodeSignal_Arcade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "01.233.161.131";
+            bool v = isIPv4Address(s);
+            Console.WriteLine(v);
+        }
+
+        static bool isIPv4Address(string inputString)
+        { 
+            List<int> list = new List<int>();
+            bool x = true;
+            int y = 0;
+            string n = "";
+            string[] st = inputString.Split('.');
+            if (st.Length == 4)
+            {
+                for (int i = 0; i < st.Length; i++)
+                {
+                    if (st[i] != "")
+                    {
+                        if (st[i].StartsWith("0") == true && st[i].Length > 1)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            x = int.TryParse(st[i], out y);
+                            if (x == true)
+                            {
+                                list.Add(Convert.ToInt32(st[i]));
+                            }
+                        }
+                    }
+
+                }
+            }
+            if (list.Count == 4)
+            {
+                foreach (var item in list)
+                {
+                    if (item > 255)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ isIPv4Address_CodeSignal_Arcade/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("isIPv4Address_CodeSignal_Arcade")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("isIPv4Address_CodeSignal_Arcade")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("6056de43-01b9-4c59-8f7e-6592c1f3d3d4")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
